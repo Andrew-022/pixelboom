3b24322aabe4e3570f0bdbda8e47cbf621aae12b
Andrew-022 <145354381+Andrew-022@users.noreply.github.com>
2024-05-06 23:25:01 +0100

    login and register auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {User} from "../model/user";
+import {Router} from "@angular/router";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FirebaseAuthService} from "../services/firebase-auth.service";
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user: User = { name: "", email: "", apellidos: "", password: ""};
+  loginMsg: string = '';
+  constructor(private router: Router,
+              private fb: FormBuilder,
+              private authService: FirebaseAuthService,
+              private dialogRef: MatDialogRef<LoginComponent>) { }
+
+  loginForm = this.fb.nonNullable.group({
+    loginMail: ['', [Validators.required]],
+    loginPwd: ['',[ Validators.required,]]
+  });
+
+  submit(){
+    const rawForm = this.loginForm.getRawValue();
+    this.authService.login(rawForm.loginMail, rawForm.loginPwd)
+      .subscribe({
+        next: () =>{
+          this.dialogRef.close();
+        },
+        error: (error) => {
+          this.loginMsg = error.code;
+        }
+      });
+  }
 
 }
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {FirebaseAuthService} from "../services/firebase-auth.service";
+import {MatDialogRef} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  message: string |null = null;
+
+  constructor(private fb: FormBuilder,
+              private authService: FirebaseAuthService,
+              private dialogRef: MatDialogRef<SignUpComponent>) {
+  }
+  registerForm = this.fb.nonNullable.group({
+    nombre: ['', [Validators.required, Validators.pattern('[a-zA-Z ]*')]],
+    apellidos: ['',[ Validators.required,Validators.pattern('[a-zA-Z ]*')]],
+    email: ['', [Validators.required, Validators.email, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
+    password: ['', [Validators.required,Validators.maxLength(16),Validators.minLength(8)]]
+  })
+
+  submit(){
+    const rawForm = this.registerForm.getRawValue();
+    this.authService.register(rawForm.email, rawForm.nombre, rawForm.apellidos, rawForm.password)
+      .subscribe({
+        next: () =>{
+          this.dialogRef.close();
+        },
+        error: (error) => {
+          this.message = error.code;
+        }
+      });
+  }
 
 }
